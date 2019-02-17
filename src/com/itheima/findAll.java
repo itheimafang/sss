a743761763bba2943ee67662180e17bd4dac07e7
zhangsan <zhangsan@itcast.cn>
2019-02-17 19:03:38 +0800

    修改

--- src/com/itheima/findAll.java
+++ src/com/itheima/findAll.java
@@ -3,5 +3,7 @@ package com.itheima;
 public class findAll {
     public static void main(String[] args) {
         System.out.println("java11");
+        //修改
+        System.out.println(123456789);
     }
 }
